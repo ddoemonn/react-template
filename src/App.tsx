578d26718543126e996cf26ffa2b2f99d72ccd93
ozergklp <ozergokalpsezer@gmail.com>
2023-10-12 00:43:48 +0300

    initialized tailwind1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import './style.css'
 export default function App() {
     return (
-        <h1  className='text-red-900 font-bold text-2xl'>I will win</h1>
+        <>
+        
+        </>
+        
     )
 }
