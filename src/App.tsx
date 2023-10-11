623c08733a973967cc509a63365116a345a69e9c
ozergklp <ozergokalpsezer@gmail.com>
2023-10-11 23:45:15 +0300

    initialized tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-
+import './style.css'
 export default function App() {
     return (
-        <h1>App</h1>
+        <h1  className='text-red-900 font-bold text-2xl'>I will win</h1>
     )
 }
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:  './src/index.html'
+        }),
+    ],
+}
