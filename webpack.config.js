296714494fe4509bfbd40a6ab25a4fe53ecc6434
ozergklp <ozergokalpsezer@gmail.com>
2023-10-28 20:25:24 +0300

    dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
     mode: 'development',
@@ -24,19 +27,28 @@ module.exports = {
                 exclude: /node_modules/,
                 use: ['style-loader', 'css-loader', 'postcss-loader'],
             },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },  
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
         ]
     },
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/',
     },
 
     plugins: [
         new HtmlWebpackPlugin({
             template:  './src/index.html'
         }),
+        new Dotenv()
     ],
-
     devServer: {
         port: 3000, 
         hot: true, 
